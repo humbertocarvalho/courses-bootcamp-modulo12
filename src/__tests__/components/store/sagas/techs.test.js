e728abba6eff16089c1d43393b37fd9ac61b841d
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-23 18:58:11 -0300

    Created axios mock func.

--- src/__tests__/components/store/sagas/techs.test.js
+++ src/__tests__/components/store/sagas/techs.test.js
@@ -1,13 +1,31 @@
 import { runSaga } from 'redux-saga';
+import MockAdapter from 'axios-mock-adapter';
+import api from '~/services/api';
 
 import { getTechs } from '~/store/modules/techs/sagas';
-import { getTechsSuccess } from '~/store/modules/techs/actions';
+import {
+  getTechsSuccess,
+  getTechsFailure
+} from '~/store/modules/techs/actions';
+
+const apiMock = new MockAdapter(api);
 
 describe('Techs Saga', () => {
   it('should be able to fecth techs', async () => {
     const dispatch = jest.fn();
+
+    apiMock.onGet('techs').reply(200, ['Node.js']);
     await runSaga({ dispatch }, getTechs).toPromise();
 
-    expect(dispatch).toHaveBeenCalledWith(getTechsSuccess());
+    expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']));
+  });
+
+  it('should fail when api returns an error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('techs').reply(500);
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
   });
 });
