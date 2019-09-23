404d6ef40044a75b51ded60713f995bd48972f5b
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-09-23 19:06:18 -0300

    Add coverage report.

--- src/__tests__/components/store/reducers/techs.test.js
+++ src/__tests__/components/store/reducers/techs.test.js
@@ -1,11 +1,17 @@
-import reducer, { INITITAL_STATE } from '~/store/modules/techs/reducer';
+import reducer, { INITIAL_STATE } from '~/store/modules/techs/reducer';
 
 import * as TechsActions from '~/store/modules/techs/actions';
 
 describe('Techs Reducer', () => {
   it('ADD_TECH', () => {
-    const state = reducer(INITITAL_STATE, TechsActions.addTech('Node.js'));
+    const state = reducer(INITIAL_STATE, TechsActions.addTech('Node.js'));
 
     expect(state).toStrictEqual(['Node.js']);
+  });
+
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {});
+
+    expect(state).toStrictEqual(INITIAL_STATE);
   });
 });
